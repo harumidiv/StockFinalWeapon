49f261ac18ee15e3a439be497f5c2f59a7a81dbd
harumidiv <h_sagawa@yumemi.co.jp>
2025-07-27 15:39:25 +0900

    プロジェクトを追加

---
+++ StockFinalWeapon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockFinalWeapon
+//
+//  Created by 佐川 晴海 on 2025/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockFinalWeapon/StockFinalWeaponApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockFinalWeaponApp.swift
+//  StockFinalWeapon
+//
+//  Created by 佐川 晴海 on 2025/07/27.
+//
+
+import SwiftUI
+
+@main
+struct StockFinalWeaponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
