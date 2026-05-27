6305afa60c9c66ce30565babfb13cf8ffc02ee3b
Harumi Sagawa <h_sagawa@yumemi.co.jp>
2026-05-27 09:15:37 +0900

    Tabを追加

---
+++ StockFinalWeapon/View/SNDKDiff/SNDKDiffScreen.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct SNDKDiffScreen: View {
+    var body: some View {
+        NavigationStack {
+            Text("SNDK差分")
+                .navigationTitle("SNDK差分")
+        }
+    }
+}
+
+#Preview {
+    SNDKDiffScreen()
+}
